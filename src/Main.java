96a97c74679b838a2ca0a09e9027cdb82f4c8455
Nante1805 <hei.nantenaina.2@gmail.com>
2023-11-19 21:28:58 +0300

    add test for book

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
-import model.Author;
-import model.Sex;
-import model.Subscriber;
+import model.*;
 import repository.AuthorCrudOperations;
+import repository.BookCrudOperations;
 import repository.SubscriberCrudOperations;
+
+import java.sql.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //author test
+        System.out.println("-".repeat(50));
         AuthorCrudOperations authorCrudOperations = new AuthorCrudOperations();
         Author authorTest = new Author("id", "Nata", Sex.M);
         authorCrudOperations.save(authorTest);
@@ -15,11 +18,21 @@ public class Main {
         authorCrudOperations.findAll().forEach(System.out::println);
 
         //subsriber test
+        System.out.println("-".repeat(50));
         SubscriberCrudOperations subscriberCrudOperations = new SubscriberCrudOperations();
         Subscriber subscriberTest = new Subscriber("id", "Visitor", "ref");
         subscriberCrudOperations.save(subscriberTest);
         subscriberCrudOperations.findAll().forEach(System.out::println);
         subscriberCrudOperations.delete(subscriberTest);
         subscriberCrudOperations.findAll().forEach(System.out::println);
+
+        //book test
+        System.out.println("-".repeat(50));
+        BookCrudOperations bookCrudOperations= new BookCrudOperations();
+        Book bookTest= new Book("id", "test", Date.valueOf("2003-03-03"), null,100,List.of(Topic.ROMANCE));
+        bookCrudOperations.save(bookTest);
+        bookCrudOperations.findAll().forEach(System.out::println);
+        bookCrudOperations.delete(bookTest);
+        bookCrudOperations.findAll().forEach(System.out::println);
     }
 }
