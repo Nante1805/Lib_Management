7be1378534353498e37cd4c8744ba3855ead27a4
Nante1805 <hei.nantenaina.2@gmail.com>
2023-11-19 20:57:57 +0300

    more test in main for subscriber

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import model.Author;
 import model.Sex;
+import model.Subscriber;
 import repository.AuthorCrudOperations;
+import repository.SubscriberCrudOperations;
 
 public class Main {
     public static void main(String[] args) {
+        //author test
         AuthorCrudOperations authorCrudOperations = new AuthorCrudOperations();
         Author authorTest = new Author("id", "Nata", Sex.M);
         authorCrudOperations.save(authorTest);
+        authorCrudOperations.findAll().forEach(System.out::println);
+        authorCrudOperations.delete(authorTest);
+        authorCrudOperations.findAll().forEach(System.out::println);
 
-        System.out.println(authorCrudOperations.findAll());
+        //subsriber test
+        SubscriberCrudOperations subscriberCrudOperations = new SubscriberCrudOperations();
+        Subscriber subscriberTest = new Subscriber("id", "Visitor", "ref");
+        subscriberCrudOperations.save(subscriberTest);
+        subscriberCrudOperations.findAll().forEach(System.out::println);
+        subscriberCrudOperations.delete(subscriberTest);
+        subscriberCrudOperations.findAll().forEach(System.out::println);
     }
 }
