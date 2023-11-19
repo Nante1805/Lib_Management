fa92262f2254c782d9c4fdbd76fe7335a3809c42
Nante1805 <hei.nantenaina.2@gmail.com>
2023-11-19 19:54:00 +0300

    init project

---
+++ resources/create_table_author.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS author (
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    sex CHAR(1) CHECK (sex IN ('M', 'F')) NOT NULL
+);
+
+INSERT INTO "author" values ('ID001', 'Mick', 'M');
+INSERT INTO "author" values ('ID002', 'Kail', 'F');
+INSERT INTO "author" values ('ID003', 'Michel', 'M');
