94f1385f1d9af14df8dfd40564fb0f2fcfb0642a
Nante1805 <hei.nantenaina.2@gmail.com>
2023-11-19 20:43:55 +0300

    finish authorCrudOperations

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import model.Author;
+import model.Sex;
 import repository.AuthorCrudOperations;
 
 public class Main {
     public static void main(String[] args) {
         AuthorCrudOperations authorCrudOperations = new AuthorCrudOperations();
+        Author authorTest = new Author("id", "Nata", Sex.M);
+        authorCrudOperations.save(authorTest);
+
         System.out.println(authorCrudOperations.findAll());
     }
 }
--- src/repository/AuthorCrudOperations.java
+++ src/repository/AuthorCrudOperations.java
@@ -4,10 +4,12 @@ import model.Author;
 import model.Sex;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ServiceLoader;
 
 public class AuthorCrudOperations implements CrudOperations<Author>{
     private final static Connection connection = ConnectionToDatabase.getConnection();
@@ -33,18 +35,32 @@ public class AuthorCrudOperations implements CrudOperations<Author>{
 
     @Override
     public List<Author> saveAll(List<Author> toSave) {
-        return null;
+        List<Author> authors = new ArrayList<>();
+        toSave.forEach(el -> {
+            authors.add(save(el));
+        });
+        return authors;
     }
 
     @Override
     public Author save(Author toSave) {
+        try{
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO author VALUES (?,?,?);");
+            statement.setString(1, toSave.getId());
+            statement.setString(2, toSave.getName());
+            statement.setString(3, toSave.getSex().toString());
+            statement.executeUpdate();
+            return toSave;
+        }catch (SQLException error){
+            System.out.println(error.getMessage());
+        }
         return null;
     }
 
     @Override
     public Author delete(Author toDelete) {
         try{
-            connection.createStatement().executeUpdate("DELET FROM author WHERE id = " + toDelete.getId());
+            connection.createStatement().executeUpdate("DELETE FROM author WHERE id = " + toDelete.getId());
             return toDelete;
         }catch (SQLException error){
             System.out.println(error.getMessage());
